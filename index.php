edf0c7f828bb32016308ab37ffa5ceef25407dae
julyagomes <julyamodena01@gmail.com>
2023-04-11 17:16:59 -0300

    navbar

--- index.php
+++ index.php
@@ -25,36 +25,10 @@ $stmt ->execute();
         </style>
         </head>
         <body>
-            <!--
-<div class="container">
-  <header class="p-3 text-bg-dark">
-  
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"/></svg>
-        </a>
-
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-dark">Features</a></li>
-          <li><a href="#" class="nav-link px-2 text-dark">Pricing</a></li>
-          <li><a href="#" class="nav-link px-2 text-dark">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-dark">About</a></li>
-        </ul>
-
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3" role="search">
-          <input type="search" class="form-control form-control-dark text-bg-dark" placeholder="Search..." aria-label="Search">
-        </form>
-
-        <div class="text-end">
-          <button type="button" class="btn btn-outline-light me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
-        </div>
-      </div>
-        </div>
-  </header>
-        -->
         <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+        <picture>
+        <img src="./img/logo.png " alt="logo" />
+      </picture>
   <a class="nav-link px-2 text-white" href="#">SISTEMA DE CADASTRO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#textoNavbar" aria-controls="textoNavbar" aria-expanded="false" aria-label="Alterna navegação">
     <span class="navbar-toggler-icon"></span>
@@ -62,13 +36,24 @@ $stmt ->execute();
   <div class="collapse navbar-collapse" id="textoNavbar">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a href="#" class="nav-link px-2 text-white">Pricing</a>
+        <a href="#" class="nav-link px-2 text-white">Início</a>
       </li>
       <li class="nav-item">
         <a href="#" class="nav-link px-2 text-white">Pricing</a>
       </li>
       <li class="nav-item">
         <a href="#" class="nav-link px-2 text-white">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link px-2 text-white dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
       </li>
         </ul>
     <span class="nav-link px-2 text-white">
